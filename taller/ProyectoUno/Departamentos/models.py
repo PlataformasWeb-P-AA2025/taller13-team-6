58b9710a315fd77d87e17453faf2a1cebd4b2dcd
Dara <dvan1@utpl.edu.ec>
2025-07-09 09:54:41 -0500

    Commit Urls

--- taller/ProyectoUno/Departamentos/models.py
+++ taller/ProyectoUno/Departamentos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Edificio(models.Model):
+    TIPO_CHOICES = [
+        ('RES', 'Residencial'),
+        ('COM', 'Comercial'),
+    ]
+
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=3, choices=TIPO_CHOICES)
+
+    def __str__(self):
+        return self.nombre
+    
+class Departamento(models.Model):
+    nombre_propietario = models.CharField(max_length=100)
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    num_cuartos = models.IntegerField()
+    edificio = models.ForeignKey(Edificio, on_delete=models.CASCADE,
+            related_name="edificio_departamentos")
+
+    def __str__(self):
+        return f"{self.numero} - {self.edificio.nombre}"
